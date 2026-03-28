8daf95e9aac591b233bbb6480ea7745e81acf5e7
Marvin Truong <khatruong06@gmail.com>
2026-03-28 23:54:42 +1000

    define interface for nodes

---
+++ internal/ast/ast.go
@@ -0,0 +1,20 @@
+package ast
+
+type Node interface {
+	TokenLiteral() string
+	String() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
