28cc5a0493d27282f9c4d66fafe7b3a531ce8e6e
Marvin Truong <khatruong06@gmail.com>
2026-03-29 00:49:50 +1000

    add program function

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -18,3 +18,18 @@ type Expression interface {
 type Program struct {
 	Statements []Statement
 }
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+func (p *Program) String() string {
+	var out string
+	for _, s := range p.Statements {
+		out += s.String()
+	}
+	return out
+}
